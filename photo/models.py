653bfc48743918e717252fd39cc7d5723bfd3c5d
modesty0310 <rlatprua4@gmail.com>
2021-10-12 11:19:19 +0900

    사진 저장하기 기능

--- photo/models.py
+++ photo/models.py
@@ -7,3 +7,15 @@ from django.db import models
 class Photo(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='timeline_photo/%Y/%M/%d')
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    # admin 화면 표시 구현
+    def __str__(self) -> str:
+        return "text : " + self.text
+
+    # 정렬 순서
+    class Meta:
+        ordering = ['-created']
