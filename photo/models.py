b0eff9a91c90257d0ca9cec8b0a099dc335dba19
modesty0310 <rlatprua4@gmail.com>
2021-10-12 09:08:23 +0900

    user model 추가

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Photo(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user')
