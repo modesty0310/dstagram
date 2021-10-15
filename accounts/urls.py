1f9607b0a595b2a5fcf03ff86ebc7a548e518a62
modesty0310 <rlatprua4@gmail.com>
2021-10-15 10:44:47 +0900

    회원가입

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
+from . import views
 
 app_name = 'accounts'
 
 # LoginView, LogoutView를 바로 template로 전달
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        usrename = request.POST.get("username")
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        # 유저 객체 생성
+        user = User()
+        user.username = usrename
+        user.set_password(password)
+        user.save()
+        return render(request, 'accounts/signup_complete.html')
+    else:
+        return render(request, 'accounts/signup.html')
